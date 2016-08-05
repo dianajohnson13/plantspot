179df8490bfa09360725f611bc1d5776f7331b17
Diana <dianajohnson13@gmail.com>
2016-08-05 14:14:48 -0700

    edit testing to use paths

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,25 +1,30 @@
 require 'test_helper'
 
-class StaticPagesControllerTest < ActionController::TestCase
+class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   
   def setup
     @base_title = "PlantSpot"
   end
 
+  test "should get root path" do
+    get root_path
+    assert_response :success
+  end
+
   test "should get home" do
-    get :home
+    get root_path
     assert_response :success
     assert_select "title", "#{@base_title}"
   end
 
   test "should get help" do
-    get :help
+    get help_url
     assert_response :success
     assert_select "title", "Help | #{@base_title}"
   end
 
   test "should get about" do
-    get :about
+    get about_url
     assert_response :success
     assert_select "title", "About | #{@base_title}"
   end
