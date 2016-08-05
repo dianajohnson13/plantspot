4c2aa5d42dbe4afeab3cf2a0bbf285e7a84c9b33
Diana <dianajohnson13@gmail.com>
2016-08-05 14:58:18 -0700

    add new user view and controller to

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -7,11 +7,14 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", root_path, count: 2
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", signup_path
   end
 
   test "layout link full_title helper" do
     get about_path
     assert_select "title", full_title("About")
+    get signup_path
+    assert_select "title", full_title("Signup")
   end
 
   # test "the truth" do
