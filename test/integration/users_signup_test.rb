d492abb684a9ab49fbecd9fcc12cc3cbd5426d51
Diana <dianajohnson13@gmail.com>
2016-08-06 12:55:06 -0700

    add and fix user signup testing, redirect vs render

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -3,16 +3,31 @@ require 'test_helper'
 class UsersSignupTest < ActionDispatch::IntegrationTest
   test "invalid signup" do
     get signup_path
-
     # checks for diff between User.count before and after submitting invalid signup
     assert_no_difference 'User.count' do
       post users_path, params: { user: {
-        name: "", email: "user@invalid",
-        password: "", password_conformation: "blah"
-        }}
+          username: "", email: "user@invalid",
+          password: "", password_conformation: "blah"
+        }
+      }
     end
-
     # checks if invalid signup triggers re-rendering of the 'users/new' page
     assert_template 'users/new'
   end
+
+  test "valid signup" do
+    get signup_path
+
+    assert_difference 'User.count' do
+      post users_path, params: { user: {
+          username: "SomeUser", email: "user@example.com",
+          password: "password1", password_conformation: "password1"
+        }
+      }
+    end
+
+    follow_redirect!
+    assert_template 'users/show'
+  end
+
 end
