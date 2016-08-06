e297a12d9d569f6577cf2df7149b2dbeec7efc8a
Diana <dianajohnson13@gmail.com>
2016-08-06 12:13:46 -0700

    create working signup form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,28 @@
 class UsersController < ApplicationController
+  
   def new
+    @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      render 'show'
+    else
+      render 'new'
+    end
+  end
+
   def show
     @user = User.find(params[:id])
   end
+
+  # PRIVATE
+  private
+
+    # user_params is for security; it limits potential data being passed from user in 'params' to be submitted
+    def user_params
+      params.require(:user).permit(:username, :email, :password, :password_confirmation)
+    end
+
 end
