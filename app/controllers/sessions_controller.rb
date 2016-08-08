8a478e419b3e62eef0e5f7a78f991063a1f29a93
Diana <dianajohnson13@gmail.com>
2016-08-07 18:43:34 -0700

    add sessions allowing users to login and logout

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,32 @@
+class SessionsController < ApplicationController
+  # inherits inclusion of SessionsHelper through ApplicationController
+  
+  def new
+    @user = User.new
+  end
+
+  def create
+    userInput = session_params
+    user = User.find_by(email: userInput[:email])
+    if user && user.authenticate(userInput[:password])
+      login(user)
+      redirect_to user
+    else 
+      render 'new'
+    end
+  end
+
+  def destroy
+    logout
+    render 'new'
+  end
+
+  # PRIVATE
+  private
+
+    # user_params is for security; it limits potential data being passed from user in 'params' to be submitted
+    def session_params
+      params.require(:session).permit(:email, :password, :user_id)
+    end
+
+end
