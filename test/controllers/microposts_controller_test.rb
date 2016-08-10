97122ff9ff2cd907f52481601c69dd19e62a7974
Diana <dianajohnson13@gmail.com>
2016-08-10 08:45:09 -0700

    add micropost routes and tests for posting/deleting when not logged in

--- test/controllers/microposts_controller_test.rb
+++ test/controllers/microposts_controller_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class MicropostsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  def setup
+    @micropost = microposts(:trees)
+  end
+
+  test "should redirect create to login when user not logged in" do
+    assert_no_difference 'Micropost.count' do
+      post microposts_path, params: {micropost: {content: "Hello!!"}}
+    end
+    assert_redirect_to login_path   
+  end
+
+  test "should redirect destroy to login when user is not logged in" do
+    assert_no_difference 'Micropost.count' do
+      post microposts_path, params: {micropost: {content: "Hello!!"}}
+    end
+    assert_redirect_to login_path
+  end
+
 end
