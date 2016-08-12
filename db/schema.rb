544bb33ea20c617aa68fa3c6ed63696a8e8a7547
Diana <dianajohnson13@gmail.com>
2016-08-11 19:38:17 -0700

    add remember_digest to users table for login

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160808224222) do
+ActiveRecord::Schema.define(version: 20160812022927) do
 
   create_table "microposts", force: :cascade do |t|
     t.text     "content"
@@ -24,9 +24,10 @@ ActiveRecord::Schema.define(version: 20160808224222) do
   create_table "users", force: :cascade do |t|
     t.string   "username"
     t.string   "email"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
     t.string   "password_digest"
+    t.string   "rememeber_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
